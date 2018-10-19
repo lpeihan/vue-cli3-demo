e66369e43b2fbeca6b7b92c61492308f6744b087
lipeihan <291630202@qq.com>
2018-10-19 22:48:54 +0800

    perf: 新增utils

---
+++ src/utils/index.js
@@ -0,0 +1,61 @@
+/*
+ * debounce
+ */
+export function debounce(fn, delay) {
+  let timer;
+  let ctx;
+  let args;
+
+  const execute = function() {
+    fn.apply(ctx, args);
+  };
+
+  return function() {
+    ctx = this;
+    args = arguments;
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(execute, delay);
+  };
+}
+
+/*
+ * throttle
+ */
+export function throttle(fn, delay) {
+  let ctx;
+  let args;
+  let timer;
+  let now;
+  let previous;
+
+  const execute = function() {
+    fn.apply(ctx, args);
+    previous = now;
+  };
+
+  return function() {
+    ctx = this;
+    args = arguments;
+    now = Date.now();
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    const diff = now - (previous + delay);
+
+    if (previous) {
+      if (diff >= 0) {
+        execute();
+      } else {
+        timer = setTimeout(execute, -diff);
+      }
+    } else {
+      execute();
+    }
+  };
+}

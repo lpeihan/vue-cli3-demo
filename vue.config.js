ec2ce3625d01e6349744a6614d055db90293a5cf
李培汉 <lipeihan@chandashi.com>
2018-10-19 11:44:14 +0800

    feat: 全局引入vars样式

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,15 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [resolve("src/assets/styles/variables.styl")]
+    });
+}
+
 module.exports = {
   baseUrl: "",
   outputDir: "dist",
@@ -15,7 +24,22 @@ module.exports = {
     config.module
       .rule("svg")
       .test(/\.svg$/)
+      .include.add(resolve("src/assets/icons"))
+      .end()
       .use("svg-sprite-loader")
-      .loader("svg-sprite-loader");
+      .loader("svg-sprite-loader")
+      .end()
+      .use("svgo-loader")
+      .loader("svgo-loader");
+
+    ["vue-modules", "vue", "normal-modules", "normal"].forEach(type =>
+      addStyleResource(config.module.rule("stylus").oneOf(type))
+    );
+  },
+
+  configureWebpack: {
+    devServer: {
+      open: true
+    }
   }
 };

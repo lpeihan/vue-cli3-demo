f50370f7afcb2b97c4f6b0ff8d4097313aad8152
李培汉 <lipeihan@chandashi.com>
2018-10-22 10:48:09 +0800

    docs: 新增注释

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from "./app";
 import router from "./router";
 import store from "./store";
 import "./service-worker";
-import "./utils/vconsole";
-// 引入全局 styles
-import "./assets/styles/index.styl";
-import fastclick from "fastclick";
+import "./utils/vconsole"; // 在href加上 ?console 就能打开移动端调试
+import "./assets/styles/index.styl"; // 引入全局 styles
+import fastclick from "fastclick"; // 解决移动端 300ms 延迟
 
 import filters from "./utils/filters";
 import mintUi from "./mint-ui";
